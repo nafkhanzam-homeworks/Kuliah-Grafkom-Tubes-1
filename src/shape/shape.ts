532515fe11556b47e16c44b2aece1c67e3437483
nafkhanzam <nafkhanalzamzami@gmail.com>
2021-02-17 21:50:44 +0700

    implement shape abstract

--- src/shape/shape.ts
+++ src/shape/shape.ts
@@ -1,3 +1,31 @@
-export interface Shape {
-  draw: () => void;
+// base abstract class
+export abstract class Shape {
+  protected program: WebGLProgram;
+
+  constructor(protected canvas: HTMLCanvasElement, protected gl: WebGLRenderingContext) {
+    this.program = gl.createProgram();
+  }
+
+  abstract render(): void;
+
+  protected createShader(shaderType: number, source: string): WebGLShader {
+    const shader = this.gl.createShader(shaderType);
+    this.gl.shaderSource(
+      shader,
+      // varying is for gradient
+      source,
+    );
+    this.gl.compileShader(shader);
+    // attach shader to the program
+    this.gl.attachShader(this.program, shader);
+    return shader;
+  }
+
+  protected linkProgram() {
+    this.gl.linkProgram(this.program);
+  }
+
+  protected useProgram() {
+    this.gl.useProgram(this.program);
+  }
 }
