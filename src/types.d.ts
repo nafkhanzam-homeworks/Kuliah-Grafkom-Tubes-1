f67465da2255523e5cb6caa3c796d81f6825036f
nafkhanzam <nafkhanalzamzami@gmail.com>
2021-02-17 22:04:44 +0700

    add color type

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,16 +1,22 @@
 type Point = [number, number];
+type Color = [number, number, number];
 
 type Line = {
   p0: Point;
   p1: Point;
+  color: Color;
 };
 
 type Square = {
   p: Point;
   size: number;
+  color: Color;
 };
 
-type Polygon = Point[];
+type Polygon = {
+  points: Point[];
+  color: Color;
+};
 
 type ShapeInstance =
   | {
